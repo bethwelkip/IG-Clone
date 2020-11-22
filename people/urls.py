b13a85c0902f3a33f9d277d1468c97868431f4d6
Bethwel Kiplimo <bkiplimo@princeton.edu>
2020-11-22 17:56:50 -0500

    urls for new functions

--- people/urls.py
+++ people/urls.py
@@ -6,10 +6,14 @@ from django.conf import settings
 
 urlpatterns = [
 url('^$', views.allprofiles, name = 'allprofiles'),
-url('^profile', views.profile, name = 'location'),
+url('^profile', views.profile, name = 'profile'),
 url(r'^search/', views.search_results, name='search_results'),
 url(r'^photo/(\d+)',views.photo,name ='photo'), 
 url(r'^auth/login',views.login,name ='login'),
 url(r'^auth/register',views.register,name ='register'),
+url(r'^comment/(\d+)',views.new_comment,name ='comment'),
+url(r'^like/(\d+)',views.new_like,name ='like'),
+url(r'^follow/(\d+)',views.follow,name ='follow'),
+url('^upload', views.upload, name = 'upload'),
 
 ]
