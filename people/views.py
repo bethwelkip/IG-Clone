0d39a5e07db218e32893fd45b6848fb282f05063
Bethwel Kiplimo <bkiplimo@princeton.edu>
2020-11-22 07:34:07 -0500

    display all profiles

--- people/views.py
+++ people/views.py
@@ -1,9 +1,11 @@
-from django.shortcuts import render, redirect
+from django.shortcuts import render, redirect, Http404
 from django.contrib.auth import authenticate, login, logout
 from django.contrib.auth.decorators import login_required
 from django.contrib import messages
 from .models import Image,Profile, Comment
 from .forms import LoginForm
+from .fillnav import initialize
+from .emails import send_welcome_email
 from django_registration.forms import RegistrationForm
 # from .forms import 
 # Create your views here.
@@ -15,7 +17,7 @@ def register(request):
         email = request.GET.get('email')
         password = request.GET.get('password')
         form.save()
-        print("Here")
+        # send_welcome_email(user,email)
         return redirect('login')
     else: 
         messages.info(request, "Username or Password is incorrect")
@@ -42,17 +44,19 @@ def login(request):
             return redirect('login')
     
     return render(request, 'auth/login.html', context)
-@login_required(login_url = '/auth/login')
+# @login_required(login_url = '/auth/login')
 def allprofiles(request):
-    posts = Image.objects.all()
-    print(posts)
+    posts = [post for post in Image.objects.all()]
+    if not posts:
+        initialize()
+    print(posts[0].image.url)
     return render(request, 'allprofiles.html', {"posts": posts})
 
 
-@login_required(login_url = '/auth/login')
+# @login_required(login_url = '/auth/login')
 def profile(request):
     pass
-@login_required(login_url = '/auth/login')
+# @login_required(login_url = '/auth/login')
 def search_results(request):
     if 'user' in request.GET and request.GET["user"]:
         search_term = request.GET.get("user")
@@ -63,16 +67,17 @@ def search_results(request):
     else:
         message = "You haven't searched for any term \n"
         return render(request, 'search.html',{"message":message})
-@login_required(login_url = '/auth/login')
-def photo(request,  photo_id):
+# @login_required(login_url = '/auth/login')
+def photo(request, photo_id):
     try:
         photo = Image.objects.get(id = photo_id)
-        coms = photo.comments.all()
-        comments = [comment.comment for comment in coms]
+        print(photo.image.url)
+        comments = photo.comments
+        # comments = [comment.comment for comment in coms]
     except:
         raise Http404()
-    return render(request, 'photo.html', {"photo": photo, "comments": comments})
-@login_required(login_url = '/auth/login')
+    return render(request, 'image.html', {"photo": photo, "comments": comments})
+# @login_required(login_url = '/auth/login')
 def new_post(request):
     current_user = request.user
     if request.method == 'POST':
@@ -84,5 +89,6 @@ def new_post(request):
         return redirect('NewsToday')
 
     else:
-        form = NewArticleForm()
+        pass
+        # form = NewArticleForm()
     return render(request, 'new_article.html', {"form": form})
